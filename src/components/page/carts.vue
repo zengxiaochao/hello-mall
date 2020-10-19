3cc0b47dca2ca09b6ca7a191dda710aa26e4afc3
zengxiaochao <zengxiaochao666@gmail.com>
2020-10-19 14:49:09 +0800

    v1.5 完善购物车页面设计实现

--- src/components/page/carts.vue
+++ src/components/page/carts.vue
@@ -16,10 +16,9 @@
         ref="multipleTable"
         :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
         @selection-change="handleSelectionChange"
-        @select="selecta"
         style="width: 100%;">
 <!--        多选框-->
-        <el-table-column align="center" type="selection" width="55"></el-table-column>
+        <el-table-column align="center"  type="selection" width="55" :selectable="canSelect"></el-table-column>
 <!--        商品图片-->
         <el-table-column label="商品" prop="img" width="110px" align="center">
           <template slot-scope="scope">
@@ -28,6 +27,12 @@
         </el-table-column>
 <!--        商品名字-->
         <el-table-column label="商品名" prop="name" align="center"></el-table-column>
+<!--        上下架状态-->
+        <el-table-column label="状态"  prop="prize" width="110px" align="center">
+          <template slot-scope="scope">
+            <el-tag :type="goodsStatus(scope.row.status)">{{scope.row.status==1?"下架":"在售"}}</el-tag>
+          </template>
+        </el-table-column>
 <!--        商品单价-->
         <el-table-column label="单价"  prop="prize" width="110px" align="center">
           <template slot-scope="scope">
@@ -37,7 +42,7 @@
 <!--        商品数量-->
         <el-table-column label="数量"  prop="num" width="140px" align="center">
           <template slot-scope="scope">
-            <el-input-number size="mini" v-model="scope.row.nums"></el-input-number>
+            <el-input-number size="mini" v-model="scope.row.nums" :disabled="scope.row.status==1?true:false"></el-input-number>
           </template>
         </el-table-column>
 <!--        商品小计-->
@@ -52,9 +57,9 @@
             <el-input v-model="search" size="mini" placeholder="输入关键字搜索"/>
           </template>
           <template slot-scope="scope">
-            <el-button size="mini" type="warning" @click="handleEdit(scope.$index, scope.row)">收藏商品</el-button>
+            <el-button size="mini" :disabled="scope.row.status==1?true:false" type="warning" @click="handleEdit(scope.$index, scope.row)">移到收藏</el-button>
             <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">移出购物车</el-button>
-            <el-button size="mini" type="success" @click="handleDelete(scope.$index, scope.row)">结算</el-button>
+            <el-button size="mini" :disabled="scope.row.status==1?true:false" type="success" @click="selected">结算</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -67,11 +72,12 @@
           <el-button type="danger" size="medium" class="button1" plain>删除选中商品</el-button>
           <el-button type="danger" size="medium" class="button1" plain>清除下架商品</el-button>
           <el-button type="warning" size="medium" class="button1" plain>移到我的收藏</el-button>
+          <el-button type="success" size="medium" class="button1" plain>结算选中商品</el-button>
           <span class="balance_ul2">
-              <span>已经选择<span class="spanText">{{allPrices}}</span>件商品</span>
-              <span>总价<span class="spanText">&yen;{{allPrices}}</span></span>
+              <span>共<span class="spanText">{{changeNumPri(1)}}</span>件商品</span>
+              <span>总价<span class="spanText">&yen;{{changeNumPri(2)}}</span></span>
               <span>
-                <el-button >去结算</el-button>
+                <el-button >全部结算</el-button>
               </span>
           </span>
         </ul>
@@ -90,11 +96,25 @@
             tableData: [{
               name: '华为P40 Pro',
               price:5988.00,
+              status:"1",
+              img:"http://05imgmini.eastday.com/mobile/20200507/20200507135939_9e1683aae3ee6fe14f853d422cdc32be_2.jpeg",
+              nums:1
+            },{
+              name: '华为P40 Pro',
+              price:5988.00,
+              status:"1",
               img:"http://05imgmini.eastday.com/mobile/20200507/20200507135939_9e1683aae3ee6fe14f853d422cdc32be_2.jpeg",
               nums:1
             }, {
               name: 'iPhone 11 Pro Max',
               price:6338.90,
+              status:"2",
+              img:"https://img12.360buyimg.com/n1/jfs/t1/68636/31/9824/169738/5d780ed7E97e88252/7b62380330636738.jpg",
+              nums:2
+            }, {
+              name: 'iPhone 11 Pro Max',
+              price:6338.90,
+              status:"2",
               img:"https://img12.360buyimg.com/n1/jfs/t1/68636/31/9824/169738/5d780ed7E97e88252/7b62380330636738.jpg",
               nums:2
             }],
@@ -110,35 +130,61 @@
           this.initChoose();
         },
         methods:{
-          selecta(selection, row)
-          {
-            this.changeNumPri();
+          canSelect(row) {
+            if(row.status==1)
+              return false;
+            else return true;
           },
-          changeNumPri()
+          goodsStatus(status)
           {
-              this.$nextTick(() => {
-                  const length = this.multipleSelection.length;
-                  //总价
-                  //全部数量
-                  let allPricess = 0.0;
-                  let allNumss = 0;
-                  for (let i = 0; i < length; i++) {
-                    allPricess += this.multipleSelection[i].nums*this.multipleSelection[i].price;
-                    allNumss += this.multipleSelection[i].nums;
-                  }
-                  this.allPrices = parseFloat(allPricess).toFixed(2);
-                  this.allNums = allPricess;
-              });
+              if(status=="1")
+                return "danger";
+              else if(status=="2")
+                return "";
+          },
+          changeNumPri(which)
+          {
+            const length = this.tableData.length;
+            //总价
+            //全部数量
+            let allPricess = 0.0;
+            let allNumss = 0;
+            for (let i = 0; i < length; i++) {
+                if(this.tableData[i].status!=1)
+                {
+                  allPricess += this.tableData[i].nums*this.tableData[i].price;
+                  allNumss += this.tableData[i].nums;
+                }
+            }
+            this.allPrices = parseFloat(allPricess).toFixed(2);
+            this.allNums = allNumss;
+            if(which=='2'){
+              return parseFloat(allPricess).toFixed(2);
+            }else{
+              return allNumss;
+            }
           },
           initChoose(){
-            this.$nextTick(() => {
+            // this.$nextTick(() => {
               //默认全选
-              this.$refs.multipleTable.toggleAllSelection();
-            });
+              // this.$refs.multipleTable.toggleAllSelection();
+            // });
           },
           // 多选操作
           handleSelectionChange(val) {
-            this.multipleSelection = val;
+            // this.multipleSelection = val;
+            // this.tableData[0].nums ++;
+            const length = val.length;
+            //总价
+            //全部数量
+            let allPricess = 0.0;
+            let allNumss = 0;
+            for (let i = 0; i < length; i++) {
+              allPricess += val[i].nums*val[i].price;
+              allNumss += val[i].nums;
+            }
+            this.allPrices = parseFloat(allPricess).toFixed(2);
+            this.allNums = allNumss;
           },
       }
     }
